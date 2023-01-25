7ab75754b9da209f4d5d982931602d3f825f1466
Santiago Báez Duic <santib1598@hotmail.com>
2023-01-25 12:19:14 -0300

    added if not exists to every table creation

--- book-ecommerce.sql
+++ book-ecommerce.sql
@@ -4,7 +4,7 @@ use ecommerce;
 /*Temática de la base de datos: ecommerce de libros
 Finalmente me decante por un ecommerce de libros porque siento que es muy sencillo encontrar ejemplos tanto para productos en sí como precios, categorías, etc.*/
 
-create table Books (
+create table if not exists Books (
 book_ID int primary key auto_increment not null unique,
 book_name varchar(50) not null,
 author varchar(30),
@@ -15,7 +15,7 @@ price int,
 stock int
 );
 
-create table Users (
+create table if not exists Users (
 user_ID int primary key auto_increment not null unique,
 user_name varchar(50) not null,
 user_surname varchar(50) not null,
@@ -25,7 +25,7 @@ address varchar(50),
 phone_number int
 );
 
-create table Admins (
+create table if not exists Admins (
 admin_ID int primary key auto_increment not null unique,
 admin_name varchar(50) not null,
 admin_surname varchar(50) not null,
@@ -33,13 +33,13 @@ email varchar(40) not null unique,
 pass varchar(15) not null
 );
 
-create table Carts (
+create table if not exists Carts (
 cart_ID int primary key auto_increment not null unique,
 cart_owner varchar(40) not null unique,
 products varchar(100)
 );
 
-create table Orders (
+create table if not exists Orders (
 order_ID int primary key auto_increment not null unique,
 order_client varchar(40) not null,
 total_price int,
