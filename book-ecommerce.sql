78503ff61ecb6f9bea9e058fa83b4bfffa220f34
Santiago Báez Duic <santib1598@hotmail.com>
2023-02-06 11:19:00 -0300

    Added data insertion

--- book-ecommerce.sql
+++ book-ecommerce.sql
@@ -1,4 +1,4 @@
-create database ecommerce;
+create database if not exists ecommerce;
 use ecommerce;
 
 /*Temática de la base de datos: ecommerce de libros
@@ -45,3 +45,31 @@ order_client varchar(40) not null,
 total_price int,
 products varchar(100)
 );
+
+/*Users*/
+insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Pedro", "Pérez", "pedroper@mail.com", "c15235", "Cabildo 1675", 1168913576);
+insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Miguel", "Fernández", "miguef@mail.com", "c12345", "Panamá 332", 1197451268);
+insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Valeria", "López", "valel@mail.com", "c68421", "Catamarca 1125", 1146218796);
+insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Daniel", "Lopez", "danilopez@mail.com", "c96512", "Uruguay 3561", 1121532354);
+insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Juliana", "Pérez", "julip@mail.com", "c99998", "Brasil 275", 1168945120);
+
+/*Admins*/
+insert into Admins (admin_name, admin_surname, email, pass) values ("Julián", "Lowe", "julianlowe@mail.com", "contra123");
+insert into Admins (admin_name, admin_surname, email, pass) values ("Romina", "Serrano", "romi_serrano@mail.com", "cafe52");
+insert into Admins (admin_name, admin_surname, email, pass) values ("Pilar", "Argit", "argitp1@mail.com", "asdfg13");
+
+/*Books*/
+insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El imperio final", "Brandon Sanderson", "Fantasy", "2006-7-17", 541, 8199, 5);
+insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("La comunidad del anillo", "J.R.Tolkien", "Fantasy", "1954-7-29", 479, 2600, 30);
+insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("A game of thrones", "George R.R. Martin", "Fantasy", "1996-8-1", 694, 9611, 10);
+insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El resplandor", "Stephen King", "Terror", "1977-1-28", 447, 6399, 20);
+insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El principito", "Saint-Exupery", "Child books", "2006-7-17", 541, 2200, 0);
+
+/*Carts*/
+insert into Carts (cart_owner, products) values ("valel@mail.com", "El principito");
+insert into Carts (cart_owner, products) values ("danilopez@mail.com", "El principito, A game of thrones");
+insert into Carts (cart_owner, products) values ("julip@mail.com", "El resplandor");
+
+/*Orders*/
+insert into Orders (order_client, total_price, products) values ("miguef@mail.com", 10799, "El imperio final, La comunidad del anillo");
+insert into Orders (order_client, total_price, products) values ("pedroper@mail.com", 14598, "El imperio final, El resplandor");
