54079e6015afceee18e1f9d21eaa76c88487eb94
Santiago Báez Duic <santib1598@hotmail.com>
2023-02-15 15:19:09 -0300

    Added comments for clarity, and two stored procedures in progress

--- book-ecommerce.sql
+++ book-ecommerce.sql
@@ -1,9 +1,14 @@
+/*Database creation and usage setting*/
 create database if not exists ecommerce;
 use ecommerce;
 
 /*Temática de la base de datos: ecommerce de libros
 Finalmente me decante por un ecommerce de libros porque siento que es muy sencillo encontrar ejemplos tanto para productos en sí como precios, categorías, etc.*/
 
+/* Al ser un script de creación e inserción utilizo tanto "if not exists/if exists" como "ignore" respectivamente para la creación e inserción para evitar posibles errores si
+se intenta correr un segmento del scripts sobre una base de datos parcialmente llena, esto no debería utilizarse al ingresar datos en una situación real, ya que detectar duplicados es útil en ese caso*/
+
+/*Books table creation*/
 create table if not exists Books (
 book_ID int primary key auto_increment not null unique,
 book_name varchar(50) not null,
@@ -15,6 +20,7 @@ price int,
 stock int
 );
 
+/*Users table creation*/
 create table if not exists Users (
 user_ID int primary key auto_increment not null unique,
 user_name varchar(50) not null,
@@ -25,6 +31,7 @@ address varchar(50),
 phone_number int
 );
 
+/*Admins table creation*/
 create table if not exists Admins (
 admin_ID int primary key auto_increment not null unique,
 admin_name varchar(50) not null,
@@ -33,12 +40,14 @@ email varchar(40) not null unique,
 pass varchar(15) not null
 );
 
+/*Carts table creation*/
 create table if not exists Carts (
 cart_ID int primary key auto_increment not null unique,
 cart_owner varchar(40) not null unique,
 products varchar(100)
 );
 
+/*Orders table creation*/
 create table if not exists Orders (
 order_ID int primary key auto_increment not null unique,
 order_client varchar(40) not null,
@@ -46,32 +55,32 @@ total_price int,
 products varchar(100)
 );
 
-/*Users*/
-insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Pedro", "Pérez", "pedroper@mail.com", "c15235", "Cabildo 1675", 1168913576);
-insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Miguel", "Fernández", "miguef@mail.com", "c12345", "Panamá 332", 1197451268);
-insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Valeria", "López", "valel@mail.com", "c68421", "Catamarca 1125", 1146218796);
-insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Daniel", "Lopez", "danilopez@mail.com", "c96512", "Uruguay 3561", 1121532354);
-insert into Users (user_name, user_surname, email, pass, address, phone_number) values ("Juliana", "Pérez", "julip@mail.com", "c99998", "Brasil 275", 1168945120);
+/*Users table insertion*/
+insert ignore into Users (user_name, user_surname, email, pass, address, phone_number) values ("Pedro", "Pérez", "pedroper@mail.com", "c15235", "Cabildo 1675", 1168913576);
+insert ignore into Users (user_name, user_surname, email, pass, address, phone_number) values ("Miguel", "Fernández", "miguef@mail.com", "c12345", "Panamá 332", 1197451268);
+insert ignore into Users (user_name, user_surname, email, pass, address, phone_number) values ("Valeria", "López", "valel@mail.com", "c68421", "Catamarca 1125", 1146218796);
+insert ignore into Users (user_name, user_surname, email, pass, address, phone_number) values ("Daniel", "Lopez", "danilopez@mail.com", "c96512", "Uruguay 3561", 1121532354);
+insert ignore into Users (user_name, user_surname, email, pass, address, phone_number) values ("Juliana", "Pérez", "julip@mail.com", "c99998", "Brasil 275", 1168945120);
 
-/*Admins*/
-insert into Admins (admin_name, admin_surname, email, pass) values ("Julián", "Lowe", "julianlowe@mail.com", "contra123");
-insert into Admins (admin_name, admin_surname, email, pass) values ("Romina", "Serrano", "romi_serrano@mail.com", "cafe52");
-insert into Admins (admin_name, admin_surname, email, pass) values ("Pilar", "Argit", "argitp1@mail.com", "asdfg13");
+/*Admins table insertion*/
+insert ignore into Admins (admin_name, admin_surname, email, pass) values ("Julián", "Lowe", "julianlowe@mail.com", "contra123");
+insert ignore into Admins (admin_name, admin_surname, email, pass) values ("Romina", "Serrano", "romi_serrano@mail.com", "cafe52");
+insert ignore into Admins (admin_name, admin_surname, email, pass) values ("Pilar", "Argit", "argitp1@mail.com", "asdfg13");
 
-/*Books*/
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El imperio final", "Brandon Sanderson", "Fantasy", "2006-7-17", 541, 8199, 5);
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("La comunidad del anillo", "J.R.Tolkien", "Fantasy", "1954-7-29", 479, 2600, 30);
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("A game of thrones", "George R.R. Martin", "Fantasy", "1996-8-1", 694, 9611, 10);
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El resplandor", "Stephen King", "Terror", "1977-1-28", 447, 6399, 20);
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El principito", "Saint-Exupery", "Child books", "2006-7-17", 541, 2200, 0);
-insert into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El camino de los reyes", "Brandon Sanderson", "Fantasy", "2010-8-31", 1007, 10799, 15);
+/*Books table insertion*/
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El imperio final", "Brandon Sanderson", "Fantasy", "2006-7-17", 541, 8199, 5);
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("La comunidad del anillo", "J.R.Tolkien", "Fantasy", "1954-7-29", 479, 2600, 30);
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("A game of thrones", "George R.R. Martin", "Fantasy", "1996-8-1", 694, 9611, 10);
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El resplandor", "Stephen King", "Terror", "1977-1-28", 447, 6399, 20);
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El principito", "Saint-Exupery", "Child books", "2006-7-17", 541, 2200, 0);
+insert ignore into Books (book_name, author, genre, release_date, page_count, price, stock) values ("El camino de los reyes", "Brandon Sanderson", "Fantasy", "2010-8-31", 1007, 10799, 15);
 
-/*Carts*/
-insert into Carts (cart_owner, products) values ("valel@mail.com", "El principito");
-insert into Carts (cart_owner, products) values ("danilopez@mail.com", "El principito, A game of thrones");
-insert into Carts (cart_owner, products) values ("julip@mail.com", "El resplandor");
+/*Carts table insertion*/
+insert ignore into Carts (cart_owner, products) values ("valel@mail.com", "El principito");
+insert ignore into Carts (cart_owner, products) values ("danilopez@mail.com", "El principito, A game of thrones");
+insert ignore into Carts (cart_owner, products) values ("julip@mail.com", "El resplandor");
 
-/*Orders*/
+/*Orders table insertion*/
 insert into Orders (order_client, total_price, products) values ("miguef@mail.com", 10799, "El imperio final, La comunidad del anillo");
 insert into Orders (order_client, total_price, products) values ("pedroper@mail.com", 14598, "El imperio final, El resplandor");
 
@@ -92,7 +101,8 @@ create or replace view short_books as
 	(select book_name, author, page_count from Books where page_count < 500);
     
 /*Functions*/
-drop function discount_price;
+drop function if exists discount_price;
+drop function if exists books_by_author;
 
 DELIMITER //
 create function discount_price(price int, discount int) returns int
@@ -103,8 +113,6 @@ set final_price = (price / 100) * (100 - discount);
 return final_price;
 end
 //
-
-drop function books_by_author;
 
 DELIMITER //
 create function books_by_author(book_author varchar(30), author varchar(30), book_name varchar(50)) returns varchar(20)
@@ -117,6 +125,41 @@ end if;
 end
 //
 
+/*Functions testing*/ 
 select discount_price(price, 20) descuento from Books;
-
 select books_by_author("brandon sanderson", author, book_name) from Books;
+
+/*Stored Procedures*/
+drop procedure if exists sort;
+drop procedure if exists deletequery;
+
+DELIMITER //
+create procedure sort(in tabla varchar(30), in sorter varchar(50),in direction varchar(4))
+begin
+	if upper(direction) = "DESC"
+    then
+	select * from tabla
+    order by sorter desc;
+    else
+    select * from tabla
+    order by sorter;
+    end if;
+end
+//
+
+DELIMITER //
+create procedure deletequery (in tabla varchar(30), in deleteid int)
+begin
+declare clausula varchar(255);
+set @clausula = concat(`delete from `,tabla,` where id=`,deleteid);
+prepare runSQL from @clausula;
+execute runSQL;
+deallocate prepare runSQL;
+end
+//
+
+/*Stored procedures testing*/
+call sort("Books", "release_date", "desc");
+call sort("Books", "release_date", "");
+
+call deletequery(Users, 3);
